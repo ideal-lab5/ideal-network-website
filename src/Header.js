a02dff4b1d47978f678685078f2495469105dcff
julianabal <julian.abal@gmail.com>
2024-06-28 09:55:11 -0300

    first commit: file structure change

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,11 @@
+import "./styles/Header.sass";
+
 function Header() {
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <a href="/docs">Docs</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="https://github.com/">GitHub</a>
-          </li>
-        </ul>
+        <a href="#" class="brand">asd</a>
       </nav>
-      <h1>Ideal Network</h1>
-      <strong>Encryption to the Future</strong>
-      <a href="https://etf.idealabs.network/docs/intro">Learn more</a>
     </header>
   );
 }
--- src/Hero.js
+++ src/Hero.js
@@ -2,7 +2,7 @@ import "./styles/Hero.sass";
 
 function Hero() {
   return (
-    <section id="hero">
+    <section id="hero" className="h-2 p-8">
       <h2>This is a Hero section</h2>
       <p>This is a Paragraph</p>
     </section>
