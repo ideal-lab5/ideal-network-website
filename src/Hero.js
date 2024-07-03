844f3057c525afef6e025c7714c39c47edd323b1
Juan Girini <juangirini@gmail.com>
2024-07-03 12:04:09 +0200

    title and subtitle

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,8 @@ function Hero() {
     <section id="hero" className="min-h-screen bg-center bg-no-repeat bg-cover">
       <div className="container flex items-center">
         <div className="textContainer flex flex-col justify-center items-start gap-10">
-          <h1 className="text-4xl xl:text-6xl font-bold">Onchain Randomness</h1>
+        <h1 className="text-4xl xl:text-6xl font-bold">Ideal Network</h1>
+        <h2 className="text-2xl xl:text-4xl font-bold">The Interoperable Entropy Layer</h2>
           <p>Onchain randomness is refreshed with each new block and can be used in smart contracts and runtime modules for random number generation.</p>
           <a href="https://etf.idealabs.network/docs/intro" className="cta btn" target="_blank" rel="noreferrer">Roll the dice</a>
         </div>
